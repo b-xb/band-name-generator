05db7aa71fc47c8aa264cff14742ddaaa8a2c573
b-xb <7443583+b-xb@users.noreply.github.com>
2021-10-25 18:33:25 +0100

    Initial planned outline of bandName object

--- bandNameGenerator.js
+++ bandNameGenerator.js
@@ -1,6 +1,22 @@
-bandName = {
+const bandName = {
+  _adjectives:["adjective"],
+  _determiners:["determiner"],
+  _nounsSingular:["singular noun"],
+  _nounsPlural:["plural noun"],
+  _prepositions:["preposition"],
+
+  get adjective() { return "word" },
+  get noun() { return "word" },
+  get nounSingular() { return "word" },
+  get nounPlural() { return "word" },
+  get preposition() { return "word" },
+  get determiner() { return "word" },
+
   generate() {
     return "Band Name"; 
+  },
+  getRandomItem(array) {
+    return "word";
   }
 }
 
