f148d8816a1d1e51f9ce3aa31378f642f5065a52
b-xb <7443583+b-xb@users.noreply.github.com>
2021-10-25 23:51:55 +0100

    combined plural and singular nouns in a single array

--- bandNameGenerator.js
+++ bandNameGenerator.js
@@ -1,14 +1,11 @@
 const bandName = {
   _adjectives:["adjective 1","adjective 2"],
   _determiners:["determiner 1","determiner 2",],
-  _nounsSingular:["singular noun 1","singular noun 2"],
-  _nounsPlural:["plural noun 1", "plural noun 2"],
+  _nouns:["singular noun 1","singular noun 2","plural noun 1", "plural noun 2"],
   _prepositions:["preposition 1", "preposition 2"],
 
   get adjective() { return this.getRandomItem(this._adjectives) },
-  get noun() { return this.getRandomItem(this._nounsSingular) },
-  get nounSingular() { return this.getRandomItem(this._nounsSingular) },
-  get nounPlural() { return this.getRandomItem(this._nounsPlural) },
+  get noun() { return this.getRandomItem(this._nouns) },
   get preposition() { return this.getRandomItem(this._prepositions) },
   get determiner() { return this.getRandomItem(this._determiner) },
 
